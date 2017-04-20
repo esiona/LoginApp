11cccbb9c02140efa170ae9f73d329b065792133
elen elen <elen elen>
2017-04-20 10:11:09 +0300

    simple form

--- application/views/layouts/main.php
+++ application/views/layouts/main.php
@@ -14,11 +14,11 @@
 
 	<div class="container">
 		<div class="col-xs-3">
-			
+			<?php $this->load->view('users/login_view'); ?> 
+
 		</div>
 		<div class="col-xs-9">
 			<?php //$this->load->view($main_view); ?> 
-			<?php $this->load->view('users/login_view'); ?> 
 
 		</div>
 	</div>
--- application/views/users/login_view.php
+++ application/views/users/login_view.php
@@ -2,5 +2,40 @@
 <?php $attributes = array('id' => 'login_form', 'class' => 'form_horizontal'); ?>
 <?php echo form_open('users/login_view', $attributes); ?>
 
+<div class="form-group">
+	<!--create username -->
+	<?php echo form_label('Username'); ?>
+	<?php
 
+	$data = array(
+		'class' => 'form-control',
+		'name' => 'username',
+		'placeholder' => 'enter name'
+	);
+	echo form_input($data);
+	 ?>
+
+	 <!--create password -->
+	<?php echo form_label('Password'); ?>
+	<?php
+
+	$data = array(
+		'class' => 'form-control',
+		'name' => 'password',
+		'placeholder' => 'enter name'
+	);
+	echo form_password($data);
+	 ?>
+
+	 <!--submit -->
+	<?php
+
+	$data = array(
+		'class' => 'btn btn-primary',
+		'name' => 'submit',
+		'value' => 'Login'
+	);
+	echo form_submit($data);
+	 ?>
+</div>
 <?php echo form_close(); ?>
