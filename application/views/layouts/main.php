81c749ff9cf470e3f55870016a6ecb8e5d3d3663
elen elen <elen elen>
2017-04-20 10:01:59 +0300

    fix paths

--- application/views/layouts/main.php
+++ application/views/layouts/main.php
@@ -4,8 +4,9 @@
 	<meta charset="UTF-8">
 	<title>Document</title>
 	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.min.css">
-	<script src="<?php echo base_url();?>assets/css/bootstrap.min.js"></script>
-	<script src="<?php echo base_url();?>assets/css/jquery-3.2.1.min.js"></script>
+	<script src="<?php echo base_url();?>assets/js/jquery-3.2.1.min.js"></script>
+
+	<script src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
 	
 </head>
 <body>
--- application/views/users/login_view.php
+++ application/views/users/login_view.php
@@ -1,1 +1,6 @@
-<p>login firm</p>
+<p>Login form</p>
+<?php $attributes = array('id' => 'login_form', 'class' => 'form_horizontal'); ?>
+<?php echo form_open('users/login_view', $attributes); ?>
+
+
+<?php echo form_close(); ?>
