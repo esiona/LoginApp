2c0605ada93c727150e9e5c3b0fa09f1b7a6ee96
elen elen <elen elen>
2017-04-20 09:52:34 +0300

    add login files

---
+++ application/views/home_view.php
@@ -0,0 +1,1 @@
+<p>Helllllloo!!!</p>
--- application/views/layouts/main.php
+++ application/views/layouts/main.php
@@ -10,5 +10,16 @@
 </head>
 <body>
 	
+
+	<div class="container">
+		<div class="col-xs-3">
+			
+		</div>
+		<div class="col-xs-9">
+			<?php //$this->load->view($main_view); ?> 
+			<?php $this->load->view('users/login_view'); ?> 
+
+		</div>
+	</div>
 </body>
 </html>
