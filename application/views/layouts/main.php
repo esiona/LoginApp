562ddf4605989b759a6c1e632e593bbe56f86514
elen elen <elen elen>
2017-04-20 09:17:39 +0300

    adding assets

---
+++ application/views/layouts/main.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.min.css">
+	<script src="<?php echo base_url();?>assets/css/bootstrap.min.js"></script>
+	<script src="<?php echo base_url();?>assets/css/jquery-3.2.1.min.js"></script>
+	
+</head>
+<body>
+	
+</body>
+</html>
