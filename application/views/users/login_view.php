10ba70ccfb8ba16c19eba32582d4461af89df2d3
elen elen <elen elen>
2017-04-20 13:58:48 +0300

    add username login

--- application/views/users/login_view.php
+++ application/views/users/login_view.php
@@ -1,58 +1,72 @@
-<p>Login form</p>
-<?php $attributes = array('id' => 'login_form', 'class' => 'form_horizontal'); ?>
+<?php if($this->session->userdata('logged_in')): ?>
 
-<?php  if($this->session->flashdata('errors')): ?>
-	<?php echo $this->session->flashdata('errors'); ?>
+	<?php if($this->session->userdata('username')): ?>
+		<?php echo $this->session->userdata('username'); ?>
+	<?php endif; ?>
+		<?php echo form_open('users/logout'); ?>
+			<?php $data = array('class' => 'btn btn-primary', 'name' => 'submit', 'value' => 'Logout' );
+			echo form_submit($data); ?>
+
+		<?php echo form_close(); ?>
+
+<?php else : ?>
+
+	<p>Login form</p>
+	<?php $attributes = array('id' => 'login_form', 'class' => 'form_horizontal'); ?>
+	<?php  if($this->session->flashdata('errors')): ?>
+		<?php echo $this->session->flashdata('errors'); ?>
+	<?php endif; ?>
+
+
+	<?php echo form_open('users/login', $attributes); ?>
+
+	<div class="form-group">
+		<!--create username -->
+		<?php echo form_label('Username'); ?>
+		<?php
+
+		$data = array(
+			'class' => 'form-control',
+			'name' => 'username',
+			'placeholder' => 'enter name'
+		);
+		echo form_input($data);
+		 ?>
+
+		 <!--create password -->
+		<?php echo form_label('Password'); ?>
+		<?php
+
+		$data = array(
+			'class' => 'form-control',
+			'name' => 'password',
+			'placeholder' => 'enter name'
+		);
+		echo form_password($data);
+		 ?>
+
+		 <!--confirm password -->
+		<?php echo form_label('Confirm Password'); ?>
+		<?php
+
+		$data = array(
+			'class' => 'form-control',
+			'name' => 'confirm_password',
+			'placeholder' => 'Confirm pass'
+		);
+		echo form_password($data);
+		 ?>
+		 <!--submit -->
+		<?php
+
+		$data = array(
+			'class' => 'btn btn-primary',
+			'name' => 'submit',
+			'value' => 'Login'
+		);
+		echo form_submit($data);
+		 ?>
+	</div>
+	<?php echo form_close(); ?>
+
 <?php endif; ?>
-
-
-<?php echo form_open('users/login', $attributes); ?>
-
-<div class="form-group">
-	<!--create username -->
-	<?php echo form_label('Username'); ?>
-	<?php
-
-	$data = array(
-		'class' => 'form-control',
-		'name' => 'username',
-		'placeholder' => 'enter name'
-	);
-	echo form_input($data);
-	 ?>
-
-	 <!--create password -->
-	<?php echo form_label('Password'); ?>
-	<?php
-
-	$data = array(
-		'class' => 'form-control',
-		'name' => 'password',
-		'placeholder' => 'enter name'
-	);
-	echo form_password($data);
-	 ?>
-
-	 <!--confirm password -->
-	<?php echo form_label('Confirm Password'); ?>
-	<?php
-
-	$data = array(
-		'class' => 'form-control',
-		'name' => 'confirm_password',
-		'placeholder' => 'Confirm pass'
-	);
-	echo form_password($data);
-	 ?>
-	 <!--submit -->
-	<?php
-
-	$data = array(
-		'class' => 'btn btn-primary',
-		'name' => 'submit',
-		'value' => 'Login'
-	);
-	echo form_submit($data);
-	 ?>
-</div>
-<?php echo form_close(); ?>
